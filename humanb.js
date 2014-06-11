a7e7e60f9488bd18e0d5faf1477c8a3a12bcdbce
Ciro S. Costa <ciro.costa@usp.br>
2014-06-10 22:57:47 -0300

    better commonjs verifier

--- humanb.js
+++ humanb.js
@@ -2,11 +2,11 @@ function humanb (b) {
   'use strict';
 
   b = parseFloat(b);
-  
+
   var t = 1024.0
    ,  units = ['KiB','MiB','GiB','TiB','PiB']
    ,  u = -1;
-  
+
   if (b < t) return b + ' B';
 
   while (b >= t)
@@ -15,5 +15,4 @@ function humanb (b) {
   return b.toFixed(1) + ' ' + units[u];
 };
 
-
-module?module.exports=humanb:'';
+if (typeof module !== 'undefined' && module.exports) module.exports = humanb;
